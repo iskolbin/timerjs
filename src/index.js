c04270b681cd5da4418a1cec39a8075a991a73ed
Ilya Kolbin <iskolbin@gmail.com>
2017-03-13 14:20:12 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+/* 
+ 
+ timerjs -- v0.3.5 public domain JS lightweight timers
+ no warranty implied; use at your own risk
+
+ Port of levent, github.com/iskolbin/ltimer
+
+ author: Ilya Kolbin (iskolbin@gmail.com)
+ url: github.com/iskolbin/timerjs
+
+ LICENSE
+
+ This software is dual-licensed to the public domain and under the following
+ license: you are granted a perpetual, irrevocable license to copy, modify,
+ publish, and distribute this file as you see fit.
+
+*/
+
 import TimerPool from './TimerPool.js'
 
 export {TimerPool}
